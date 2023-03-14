60761df2974ab030d702ff835227188fc34bb35e
Photi31 <photiniya@gmail.com>
2023-03-14 10:41:38 +0300

    Added AppBar

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 import {TaskType, Todolist} from './Todolist';
 import {v1} from 'uuid';
 import {AddItemForm} from './AddItemForm';
+import {AppBar, Button, IconButton, Toolbar, Typography} from "@mui/material";
+import {Menu} from "@mui/icons-material";
 
 export type FilterValuesType = "all" | "active" | "completed";
 type TodolistType = {
@@ -118,6 +120,17 @@ function App() {
 
     return (
         <div className="App">
+            <AppBar position='static'>
+                <Toolbar>
+                    <IconButton edge='start' color='inherit' aria-label='menu'>
+                        <Menu/>
+                    </IconButton>
+                    <Typography variant='h6'>
+                        Todolist
+                    </Typography>
+                    <Button color='inherit'>Login</Button>
+                </Toolbar>
+            </AppBar>
             <AddItemForm addItem={addTodolist} />
             {
                 todolists.map(tl => {
